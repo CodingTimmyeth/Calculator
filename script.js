096e2fb37ea4d867bdc610c81b7264d46756731c
Tommy Miguel <tommymiguel@Tommys-MacBook-Pro.local>
2023-04-10 15:27:28 -0700

    Functionality ✅, need to do minor fixes

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
-// Variable to show the Math on the DOM
 const screen = document.querySelector('.screen')
-// Variables for Buttons
 const buttons = document.querySelectorAll('button')
 
-console.log(buttons);
-
-// Functions for Math Operations
-
-const multiply = (a, b) => {return a * b}
-const divide = (a, b) => {return a / b}
-const add  = (a, b) => {return a + b}
-const subtract = (a, b) => {return a - b}
-
-// Function to reset the Screen
-const resetCalculator = () => {
-    screen.textContent = ''
-}
+let input = [];
 
 buttons.forEach((button) => {
-
-    button.addEventListener('click', () => {
-        console.log(button.innerText)
-        screen.textContent += ` ${button.innerText}`
-    })
-
   button.addEventListener('click', () => {
-    if(button.value === 'equal'){
-        console.log('run equal sign')
+    const value = button.innerText;
+
+    if (!isNaN(value)) {
+      input.push(value);
+      screen.textContent += value;
     }
 
-    if(button.innerText === 'Clear'){
-        resetCalculator()
+    if (['+', '-', '*', '/', '%'].includes(value)) {
+      input.push(value);
+      screen.textContent += ` ${value} `;
     }
-  })
-})
+
+    if (button.innerText === '=') {
+      let result = null;
+      let num1 = null;
+      let operator = null;
+
+      input.forEach((value) => {
+        if (!isNaN(value)) {
+          if (num1 === null) {
+            num1 = parseInt(value);
+          } else {
+            switch (operator) {
+              case '+':
+                num1 += parseInt(value);
+                break;
+              case '-':
+                num1 -= parseInt(value);
+                break;
+              case '*':
+                num1 *= parseInt(value);
+                break;
+              case '/':
+                num1 /= parseInt(value);
+                break;
+              case '%':
+                num1 %= parseInt(value);
+                break;
+            }
+          }
+        } else {
+          operator = value;
+        }
+      });
+
+      screen.textContent = num1;
+      input = [num1];
+    }
+
+    if (value === 'Clear') {
+      screen.textContent = '';
+      input = [];
+    }
+  });
+});
